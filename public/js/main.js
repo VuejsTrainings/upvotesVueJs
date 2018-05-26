a21b5c36ddf457dc0ace20ddf8a3b0d2bf6660cb
Eric Guyader <ericguyader@yahoo.fr>
2018-05-26 11:02:14 +0200

    Sépare la logique des propositions dans un composant

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,43 @@
-//import submissions from './seed';
+const submissionComponent = {
+    props : ['submission'],
+    methods: {
+        augmentVotes(submission){
+            submission.votes++;
+        }
+    },
+    template:`
+    <div style="display:flex;width:100%">
+        <figure class="media-left">
+            <img v-bind:src="submission.picture" alt="" class="image is-64x64">
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h4>
+                    <a v-bind:href="submission.url" class="has-text-info">{{ submission.title }}</a>
+                    <span class="tag is-small">#{{ submission.id }}</span>
+                </h4>
+                <p>
+                    {{ submission.description }}
+                </p>
+                <p>
+                    <small class="is-size-7">
+                        Submitted by:
+                        <img v-bind:src="submission.avatar" alt="" class="image is-24x24">
+                    </small>
+                </p>
+            </div>
+        </div>
+        <div class="media-right">
+            <span class="icon is-small">
+                <i class="fa fa-chevron-up" v-on:click="augmentVotes(submission)"></i>
+                <strong class="has-text-info">{{ submission.votes }}</strong>
+            </span>
+        </div>
+    </div>
+    `
+};
+
+
 new Vue({
     el:'#app',
     data: {
@@ -9,9 +48,7 @@ new Vue({
             return this.submissions.sort( (a,b) => b.votes - a.votes )
         }
     },
-    methods: {
-        augmentVotes(submission){
-            submission.votes++;
-        }
+    components : {
+        'submission-component':submissionComponent
     }
 });
